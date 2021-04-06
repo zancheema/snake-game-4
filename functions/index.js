0965c0adf10c495bc617dadc118204b8e1de36f3
= <social.zaincheema@gmail.com>
2021-04-06 13:46:45 +0500

    added unreadMessagesCount

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require("firebase-functions");
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
 admin.initializeApp();
+const db = admin.firestore();
 
 exports.sendNotification = functions.firestore
     .document("users/{userId}/notifications/{notificationId}")
@@ -22,4 +23,23 @@ exports.sendNotification = functions.firestore
       };
 
       admin.messaging().sendToDevice(token, payload, options);
+    });
+
+exports.unreadMessagesCount = functions.firestore
+    .document("users/{userId}/chats/{chatId}/messages/{messageId}")
+    .onCreate(async (snap, context) => {
+      // return if the message is sent by the same user
+      if (snap.data().mine) return;
+
+      const uid = context.params.userId;
+      const unreadMessagesDoc =
+        db.doc(`users/${uid}/chatFunctions/unreadMessages`);
+
+      try {
+        const unreadMessages = await unreadMessagesDoc.get();
+        const count = unreadMessages.data().totalCount;
+        await unreadMessagesDoc.set({totalCount: count + 1});
+      } catch (e) {
+        await unreadMessagesDoc.set({totalCount: 1});
+      }
     });
